90897b7feb535a84eb3ea86c3aa239eadadc83dc
katie-roberts <katie.roberts@bbc.co.uk>
2016-07-25 14:24:30 +0100

    wired up the build with the motors and leds and scroll all in one place.

--- buildBunny.js
+++ buildBunny.js
@@ -4,26 +4,122 @@ var board = new five.Board();
 
 board.on("ready", function () {
 
-  var button = new five.Button(9);
+  var button = new five.Button(3);
   var scroll = new five.Sensor("A0");
-  var led = new five.Led(11);
+  var redLed = new five.Led(10);
+  var amberLed = new five.Led(12);
+  var greenLed = new five.Led(11);
+
+  var leftEar;
+  var rightEar;
 
 
-//  button.on('press', function(){
-//    console.log('button pressed');
-//    led.on();
-//  });
-//
-//  button.on('release', function(){
-//    console.log('button released');
-//    led.off();
-//  });
-
-  scroll.scale(0, 255).on('change', function () {
-    console.log('The scaled potentiometer value is ', this.value);
-    console.log('The raw value ', this.raw);
-    led.brightness(this.value);
+  leftEar = new five.Motor({
+    pins: {
+      pwm: 9,
+      dir: 8,
+      enable: 7
+    },
+    invertPWM: true
   });
 
+  rightEar = new five.Motor({
+    pins: {
+      pwm: 5,
+      dir: 4,
+      enable: 2
+    },
+    invertPWM: true
+  });
+
+  board.repl.inject({
+    amberLed: amberLed,
+    greenLed: greenLed,
+    redLed: redLed,
+    leftEar: leftEar,
+    rightEar: rightEar
+  });
+
+
+  button.on('press', function(){
+    console.log('button pressed');
+ //   redLed.on();
+  });
+
+  button.on('release', function(){
+    console.log('button released');
+ //   redLed.off();
+  });
+
+  scroll.scale(0, 255).on('change', function () {
+   // console.log('The scaled potentiometer value is ', this.value);
+ //   console.log('The raw value ', this.raw);
+   // redLed.brightness(this.value);
+   // amberLed.brightness(this.value);
+  });
+
+
+  leftEar.on("stop", function() {
+    console.log("automated stop on timer", Date.now());
+  });
+
+  leftEar.on("forward", function() {
+    console.log("forward", Date.now());
+
+    // enable the motor after 2 seconds
+    board.wait(2000, function() {
+      leftEar.enable();
+    });
+  });
+
+  leftEar.on("enable", function() {
+    console.log("motor enabled", Date.now());
+
+    // enable the motor after 2 seconds
+    board.wait(2000, function() {
+      leftEar.stop();
+    });
+  });
+
+  leftEar.on("disable", function() {
+    console.log("motor disabled", Date.now());
+  });
+
+
+  rightEar.on("stop", function() {
+    console.log("automated stop on timer", Date.now());
+  });
+
+  rightEar.on("forward", function() {
+    console.log("forward", Date.now());
+
+    // enable the motor after 2 seconds
+    board.wait(2000, function() {
+      rightEar.enable();
+    });
+  });
+
+  rightEar.on("enable", function() {
+    console.log("motor enabled", Date.now());
+
+    // enable the motor after 2 seconds
+    board.wait(2000, function() {
+      rightEar.stop();
+    });
+  });
+
+  rightEar.on("disable", function() {
+    console.log("motor disabled", Date.now());
+  });
+
+
+  // disable the motor
+  leftEar.disable();
+  rightEar.disable();
+
+  // set the motor going forward full speed (nothing happen)
+  leftEar.forward(255);
+  rightEar.forward(255);
+  
 });
 
--- motors.js
+++ motors.js
@@ -2,11 +2,11 @@ var five = require("johnny-five"),
   board = new five.Board();
 
 board.on("ready", function() {
-  var leftMotor;
-  var rightMotor;
+  var leftEar;
+  var rightEar;
 
 
-  leftMotor = new five.Motor({
+  leftEar = new five.Motor({
     pins: {
       pwm: 9,
       dir: 8,
@@ -15,7 +15,7 @@ board.on("ready", function() {
     invertPWM: true
   });
 
-  rightMotor = new five.Motor({
+  rightEar = new five.Motor({
     pins: {
       pwm: 5,
       dir: 4,
@@ -25,69 +25,69 @@ board.on("ready", function() {
   });
 
   board.repl.inject({
-    leftMotor: leftMotor,
-    rightMotor: rightMotor
+    leftEar: leftEar,
+    rightEar: rightEar
   });
 
-  leftMotor.on("stop", function() {
+  leftEar.on("stop", function() {
     console.log("automated stop on timer", Date.now());
   });
 
-  leftMotor.on("forward", function() {
+  leftEar.on("forward", function() {
     console.log("forward", Date.now());
 
     // enable the motor after 2 seconds
     board.wait(2000, function() {
-      leftMotor.enable();
+      leftEar.enable();
     });
   });
 
-  leftMotor.on("enable", function() {
+  leftEar.on("enable", function() {
     console.log("motor enabled", Date.now());
 
     // enable the motor after 2 seconds
     board.wait(2000, function() {
-      leftMotor.stop();
+      leftEar.stop();
     });
   });
 
-  leftMotor.on("disable", function() {
+  leftEar.on("disable", function() {
     console.log("motor disabled", Date.now());
   });
 
 
-  rightMotor.on("stop", function() {
+  rightEar.on("stop", function() {
     console.log("automated stop on timer", Date.now());
   });
 
-  rightMotor.on("forward", function() {
+  rightEar.on("forward", function() {
     console.log("forward", Date.now());
 
     // enable the motor after 2 seconds
     board.wait(2000, function() {
-      rightMotor.enable();
+      rightEar.enable();
     });
   });
 
-  rightMotor.on("enable", function() {
+  rightEar.on("enable", function() {
     console.log("motor enabled", Date.now());
 
     // enable the motor after 2 seconds
     board.wait(2000, function() {
-      rightMotor.stop();
+      rightEar.stop();
     });
   });
 
-  rightMotor.on("disable", function() {
+  rightEar.on("disable", function() {
     console.log("motor disabled", Date.now());
   });
 
 
   // disable the motor
-  leftMotor.disable();
-  rightMotor.disable();
+  leftEar.disable();
+  rightEar.disable();
 
   // set the motor going forward full speed (nothing happen)
-  leftMotor.forward(255);
-  rightMotor.forward(255);
+  leftEar.forward(255);
+  rightEar.forward(255);
 });
