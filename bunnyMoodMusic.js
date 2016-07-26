bf8f945540f695e1c0160336e1ed66263618b0ef
intih <inti.herteleer@bbc.co.uk>
2016-07-26 13:55:27 +0100

    Play tune only if happiness percentage has changed

--- bunnyMoodMusic.js
+++ bunnyMoodMusic.js
@@ -2,38 +2,44 @@ var five = require("johnny-five");
 var songs = require('j5-songs');
 var board = new five.Board();
 
-var areWeHappy = require('./slackIntegration/calculateMood').areWeHappy;
+var howHappy = require('./slackIntegration/calculateMood').howHappy;
 
 board.on("ready", function() {
     var piezo = new five.Piezo(9);
     var happySong = songs.load('mario-intro');
     var sadSong = songs.load('funeral-march-short');
+    var happyPercentage = -1;
 
     this.repl.inject({
         piezo: piezo
     });
 
-    var playMoodSong = function (happy) {
-        if (happy) {
-            var happySongClone = JSON.parse(JSON.stringify(happySong));
-            console.log('We are happy!');
-            piezo.play(happySong);
-            happySong = happySongClone;
-        } else {
-            var sadSongClone = JSON.parse(JSON.stringify(sadSong));
-            console.log('We are sad :(');
-            piezo.play(sadSong);
-            sadSong = sadSongClone;
+    var playMoodSong = function (newHappyPercentage) {
+        console.log('newHappyPercentage: ', newHappyPercentage);
+        if (newHappyPercentage !== happyPercentage){
+            happyPercentage = newHappyPercentage;
+
+            if (happyPercentage > 50) {
+                var happySongClone = JSON.parse(JSON.stringify(happySong));
+                console.log('We are happy!');
+                piezo.play(happySong);
+                happySong = happySongClone;
+            } else {
+                var sadSongClone = JSON.parse(JSON.stringify(sadSong));
+                console.log('We are sad :(');
+                piezo.play(sadSong);
+                sadSong = sadSongClone;
+            }
         }
     }
 
     // Load a song object
     setInterval(
         function() {
-            areWeHappy(playMoodSong);
+            howHappy(playMoodSong);
         },
-        30*1000
+        5*1000
     );
 
-    areWeHappy(playMoodSong);
+    howHappy(playMoodSong);
 });
